bbc9af27e3a1187267bc659038934f31a6bf0f13
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2023-01-05 08:25:07 -0600

    added the intial repo

---
+++ explore-kafka-streams-using-spring-boot/orders-management-streams/src/main/java/com/learnkafkastreams/OrdersManagementStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.learnkafkastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersManagementStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersManagementStreamsApplication.class, args);
+	}
+
+}
