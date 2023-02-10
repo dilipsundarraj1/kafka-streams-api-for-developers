884549450f2de46112938a3d48556b3efad996c1
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2023-02-09 21:02:48 -0600

    added the missing code

--- explore-kafka-streams-using-spring-boot/orders-management-streams/orders-streams-app/src/main/java/com/learnkafkastreams/topology/OrdersTopology.java
+++ explore-kafka-streams-using-spring-boot/orders-management-streams/orders-streams-app/src/main/java/com/learnkafkastreams/topology/OrdersTopology.java
@@ -1,8 +1,12 @@
 package com.learnkafkastreams.topology;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+@Slf4j
 public class OrdersTopology {
 
     public static final String ORDERS = "orders";
