8945a437069f2d549cbf2602acffb9ede71aa50b
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2023-02-19 05:25:45 -0600

    added the initial test for the orders kafka streams;

--- explore-kafka-streams-using-spring-boot/orders-management-streams/orders-domain/src/main/java/com/learnkafkastreams/domain/Order.java
+++ explore-kafka-streams-using-spring-boot/orders-management-streams/orders-domain/src/main/java/com/learnkafkastreams/domain/Order.java
@@ -10,52 +10,5 @@ public record Order(Integer orderId,
                     OrderType orderType,
                     List<OrderLineItem> orderLineItems,
                     LocalDateTime orderedDateTime) {
-    public static record Order(Integer orderId,
-                               String locationId,
-                               BigDecimal finalAmount,
-                               OrderType orderType,
-                               List<OrderLineItem> orderLineItems,
-                               LocalDateTime orderedDateTime) {
-    }
 
-    public static record OrderLineItem(
-            String item,
-            Integer count,
-            BigDecimal amount) {
-    }
-
-    public enum OrderType {
-        GENERAL,
-        RESTAURANT
-    }
-
-    public static record Revenue(String locationId,
-                                 BigDecimal finalAmount) {
-    }
-
-    public static record Store(String locationId,
-                               Address address,
-                               String contactNum) {
-    }
-
-    public static record TotalRevenue(String locationId,
-                                      Integer runnuingOrderCount,
-                                      BigDecimal runningRevenue) {
-
-        public TotalRevenue() {
-            this("", 0, BigDecimal.valueOf(0.0));
-        }
-
-        public TotalRevenue updateRunningRevenue(String key, com.learnkafkastreams.domain.Order order) {
-
-            var newOrdersCount = this.runnuingOrderCount+1;
-            var newRevenue = this.runningRevenue.add( order.finalAmount());
-            return new TotalRevenue(key, newOrdersCount, newRevenue);
-
-        }
-    }
-
-    public static record TotalRevenueWithAddress(com.learnkafkastreams.domain.TotalRevenue totalRevenue,
-                                                 com.learnkafkastreams.domain.Store store) {
-    }
 }
--- explore-kafka-streams-using-spring-boot/orders-management-streams/orders-streams-app/src/test/java/com/learnkafkastreams/topology/OrdersTopologyTest.java
+++ explore-kafka-streams-using-spring-boot/orders-management-streams/orders-streams-app/src/test/java/com/learnkafkastreams/topology/OrdersTopologyTest.java
@@ -1,4 +1,68 @@
 package com.learnkafkastreams.topology;
 
-public class OrdersTopologyTest {
+import com.learnkafkastreams.domain.*;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.TestInputTopic;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.kafka.support.serializer.JsonSerde;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.learnkafkastreams.topology.OrdersTopology.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OrdersTopologyTest {
+
+    TopologyTestDriver topologyTestDriver = null;
+    TestInputTopic<String, Order> ordersInputTopic = null;
+
+    static String INPUT_TOPIC = ORDERS;
+
+    static List<KeyValue<String, Order>> orders(){
+
+        var orderItems = List.of(
+                new OrderLineItem("Bananas", 2, new BigDecimal("2.00")),
+                new OrderLineItem("Iphone Charger", 1, new BigDecimal("25.00"))
+        );
+
+        var orderItemsRestaurant = List.of(
+                new OrderLineItem("Pizza", 2, new BigDecimal("12.00")),
+                new OrderLineItem("Coffee", 1, new BigDecimal("3.00"))
+        );
+
+        var order1 = new Order(12345, "store_1234",
+                new BigDecimal("27.00"),
+                OrderType.GENERAL,
+                orderItems,
+                LocalDateTime.now()
+                //LocalDateTime.now(ZoneId.of("UTC"))
+        );
+
+        var order2 = new Order(54321, "store_1234",
+                new BigDecimal("15.00"),
+                OrderType.RESTAURANT,
+                orderItemsRestaurant,
+                LocalDateTime.now()
+                //LocalDateTime.now(ZoneId.of("UTC"))
+        );
+        var keyValue1 = KeyValue.pair( order1.orderId().toString()
+                , order1);
+
+        var keyValue2 = KeyValue.pair( order2.orderId().toString()
+                , order2);
+
+
+        return  List.of(keyValue1, keyValue2);
+
+    }
 }
